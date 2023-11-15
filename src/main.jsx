7e44c73de8a3affeda3cd92398de0a0472fe8840
ysfuzmn <ysfuzmn2077@gmail.com>
2023-11-15 13:26:20 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>
+</React.StrictMode>,
+);
+
